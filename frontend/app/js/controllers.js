677b02340b6b00056c2f594d4bfff65f9ab9c853
Aamir Rasheed <aamir1rasheed@gmail.com>
2015-01-18 01:40:50 -0500

    Basic layout finished

--- frontend/app/js/controllers.js
+++ frontend/app/js/controllers.js
@@ -4,13 +4,36 @@
 
 var phonecatApp = angular.module('familyApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope) {
-  $scope.phones = [
-    {'name': 'Nexus',
-     'snippet': 'Fast just got faster with Nexus.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+phonecatApp.controller('FamilyCardsCtrl', function($scope) {
+	$scope.users = [
+		{'firstName': 'Aamir',
+     	 'lastName': 'Rasheed',
+     	 'snippet': 'Fast just got faster with Nexus.'}
+   	];
+	$scope.parents = [
+	    {'firstName': 'Dora',
+	     'lastName': 'Li',
+	     'snippet': 'Fast just got faster with Nexus.'},
+	    {'firstName': 'Bryan',
+	     'lastName':'Yan',
+	     'snippet': 'The Next, Next Generation tablet.'},
+	     
+	];
+	$scope.children = [
+	    {'firstName': 'Vaishnavi',
+	     'lastName': 'Raman',
+	     'snippet': 'Fast just got faster with Nexus.'},
+	    {'firstName': 'Jason',
+	     'lastName':'Wu',
+	     'snippet': 'The Next, Next Generation tablet.'},
+	     {'firstName': 'Frederick',
+	     'lastName':'Lee',
+	     'snippet': 'The Next, Next Generation tablet.'},
+	];
+	$scope.spouses = [
+	    {'firstName': 'Youngjin',
+	     'lastName': 'Yun',
+	     'snippet': 'Fast just got faster with Nexus.'},
+	];
+
 });
